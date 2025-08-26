0e5381d65bf61047c5766af55db1f6ddc22c9c03
EstephanZ246 <por19826@uvg.edu.gt>
2025-08-26 04:39:23 -0600

    Inicio Programacion

---
+++ Programacion/CarroCompetenciaWRO/FEPO.h
@@ -0,0 +1,22 @@
+/*Libreria creada por: Estephan Portales
+* Basandose en librerias existentes para MPU6050
+* 
+*/
+#ifndef FEPO
+#define FEPO
+#include <Arduino.h>
+
+void inicioMPU();
+void MPUwrite(byte reg, byte datas);
+byte MPUread(byte reg);
+void calcoffset(bool console);
+void mpuupdate();
+
+
+extern float roll, pitch, yaw, gyroX, gyroY;
+
+
+
+#endif
+
+
