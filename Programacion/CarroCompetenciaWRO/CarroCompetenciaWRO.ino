79236003e9f15ca2bf2d4d6b055bc879e0c786c5
EstephanZ246 <por19826@uvg.edu.gt>
2025-08-26 05:00:43 -0600

    Programacion Servo

--- Programacion/CarroCompetenciaWRO/CarroCompetenciaWRO.ino
+++ Programacion/CarroCompetenciaWRO/CarroCompetenciaWRO.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+// Librerías
 
+#include "FEPO.h"
+#include <Servo.h>
+
+// Constantes (Definimos en dónde conectamos todo)
+#define ServosRuedas
+
+// Definir Objetos
+Servo ServoDireccion;            // Creamos un objeto servo
+Servo ServoUltrasonicoAdelante;  // Creamos un objeto servo
+Servo ServoUltrasonicoAtras;     // Creamos un objeto servo
+
+void setup() {
+  inicioMPU();
+  Serial.begin(9600);
+
+  ServoDireccion.attach(9);  // Pin donde está conectado el servo (PWM)
+  ServoUltrasonicoAdelante.attach(10);  // Pin donde está conectado el servo (PWM)
+  ServoUltrasonicoAtras.attach(11);  // Pin donde está conectado el servo (PWM)
+  //Posiciones iniciales
+  ServoDireccion.write(90);
+  ServoUltrasonicoAdelante.write(90);
+  ServoUltrasonicoAtras.write(90);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  mpuupdate();
+  Serial.println((int)yaw);
+  delay(10);
 }
